fe025d512d3f2fe628fa37ba0926d63f18daf6f3
Esso4real <81115661+Esso4real@users.noreply.github.com>
2021-09-04 00:11:46 -0600

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,16 @@
+/* def buildJar() {
+  
+} */
+
+def buildImage() {
+    echo "Building Docker Image"
+    withCredentials([usernamePassword(credentialsId: 'hub-docker', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+                        sh "docker build -t esso4real/myhtmlapp:Tek-Experts-1.0 ."
+                        sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin"
+                        sh "docker push esso4real/myhtmlapp:Tek-Experts-1.0"
+    }
+}
+
+def deployApp() {
+  echo "Deploying App"
+}
